cc5e0edfeb41fb63dae79f93fab7d4e29936fc41
Pabl0Parra <pabsblue16@gmail.com>
2022-11-04 18:59:53 +0100

    initialize project structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.scss";
+import Todos from "./components/Todos";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
