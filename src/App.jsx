a1341ebe2823c4b72040b54babfca211191fb05f
Pabl0Parra <pabsblue16@gmail.com>
2022-11-04 19:31:47 +0100

    added footer & imgs for footer & header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.scss";
 import Todos from "./components/Todos";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const theme = useStore((state) => state.theme);
@@ -12,6 +13,7 @@ function App() {
   });
   return (
     <div className={AppClasses}>
+      <Header />
       <Todos />
     </div>
   );
